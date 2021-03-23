4ab4541736c1d8b05b57b6264132e05b4e76c673
lucianolopez85 <lopezz.lucio85@gmail.com>
2021-03-23 16:57:11 -0300

    add fragments do menu, editado lista no home

--- app/src/main/java/com/example/academia/MainActivity.kt
+++ app/src/main/java/com/example/academia/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.academia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.example.academia.Fragments.Avaliacao
+import com.example.academia.Fragments.Biblioteca
+import com.example.academia.Fragments.Home
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var Content: FrameLayout? = null
+
+    private var mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when(item.itemId){
+            R.id.nav_inicio ->{
+                val fragment = Home.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+
+            }
+
+            R.id.nav_buscar ->{
+                val fragment = Avaliacao.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_biblioteca ->{
+                val fragment = Biblioteca.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Content = content
+        bottom_menu.setOnNavigationItemSelectedListener (mOnNavigationItemSelectedListener )
+
+        val fragment = Home.newInstance()
+        addFragment(fragment)
+
+    }
+    private fun addFragment(fragment: Fragment){
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content, fragment, fragment.javaClass.simpleName)
+                .commit()
     }
 }
